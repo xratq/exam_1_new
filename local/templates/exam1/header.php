2b5eb9806e8ec93a522a0160a692e89f23055196
xratq <44581405+xratq@users.noreply.github.com>
2024-07-16 10:32:26 +0300

    Update header.php

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -114,7 +114,7 @@ IncludeTemplateLangFile(__FILE__);
                     <div class="cnt">
                         <?if ($APPLICATION->GetCurPage(false) != "/"):?>
                     <header>
-                            <h1><?$APPLICATION->ShowTitle()?></h1>
+                            <h1><?$APPLICATION->ShowTitle(false)?></h1>
                         </header>
                         <hr>
                         <?else:?>
